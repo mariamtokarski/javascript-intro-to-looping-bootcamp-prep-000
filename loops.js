ede1d4d47479d00fbeb39b8f1d4a5dabe3a49ed5
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-23 22:58:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,20 @@
 function forLoop (array){
   for (var i = 0; i < 25; i++) {
-  if (i===1) {
+    if (i===1) {
     console.log(array.push("I am 1 strange loop."))
-  } else if (i===0) {
+    } else if (i===0) {
     console.log(array.push("I am ${i} strange loop."))
-  } else {
+    } else {
     console.log(array.push("I am ${i} strange loops."))
+    }
   }
-}
   return array
+}
+
+function whileLoop (number){
+  let number = 10
+  while (number > 0) {
+    console.log(--number)
+  }
+  return "done"
 }
